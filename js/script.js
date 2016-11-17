8d78a12b4b41a337de35933767a57eebfd97109e
Yulia <rout66@bk.ru>
2016-11-18 01:18:50 +0300

    внесены изменения

--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
 	var link = document.querySelector(".btn-feedback");
 	var popup = document.querySelector(".write-us");
 	var close = document.querySelector(".write-us-close");
-	var fullname = popup.querySelector("[name=fullname]");
+	var fullname = document.querySelector("[name=fullname]");
 
 	var openmap = document.querySelector(".open-map");
 	var map = document.querySelector(".modal-map");
-	var closemap = map.querySelector(".modal-map-close");
+	var closemap = document.querySelector(".modal-map-close");
 
 	var buy = document.querySelector(".buy");
 	var cart = document.querySelector(".cart");
 	var closecart = document.querySelector(".cart-close");
 
-link.addEventListener("click", function(event) {
+if (link) {
+	link.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.add("write-us-show");
 	fullname.focus();
 });
+}
+
+if (close) {
 	close.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.remove("write-us-show");
 });
-
+}
+if (buy) {
 	buy.addEventListener("click", function(event) {
 	event.preventDefault();
 	cart.classList.add("cart-show");
 });
+}
+
+if (closecart) {
 	closecart.addEventListener("click", function(event) {
 	event.preventDefault();
 	cart.classList.remove("cart-show");
 });
+}
 
+if (openmap) {
 	openmap.addEventListener("click", function(event) {
 	event.preventDefault();
 	map.classList.add("modal-map-show");
 });
+}
+
+if (closemap) {
 	closemap.addEventListener("click", function(event) {
 	event.preventDefault();
 	map.classList.remove("modal-map-show");
 });
+}
+
 	window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
 	if (map.classList.contains("modal-map-show")) {
